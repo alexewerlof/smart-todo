38c523f6c179ceaf9d1564539f2b38f560253b12
userpixel <hanifbbz@gmail.com>
2016-07-31 22:55:24 +0200

    we got some basic styling in place :-)

--- src/pages/home/page.js
+++ src/pages/home/page.js
@@ -3,7 +3,7 @@ import styles from "./style.css";
 import $ from "jquery";
 
 export default class HomePage extends React.Component {
-  render() {
+  render () {
     return (
       <div className={styles.content}>
         <h1>Home Page</h1>
@@ -16,20 +16,20 @@ export default class HomePage extends React.Component {
 
 class TaskList extends React.Component {
 
-  constructor() {
+  constructor () {
     super();
     this.state = {
       tasks: []
     };
   }
 
-  componentDidMount() {
+  componentDidMount () {
     $.getJSON('/tasks')
     .done(data => this.setState({tasks: data.tasks}))
     .fail(function(e) { console.dir(e); });
   }
 
-  render() {
+  render () {
     return (
       <div>
         <div>
@@ -45,13 +45,38 @@ class TaskList extends React.Component {
           </tr>
         </thead>
         <tbody>
-          {this.state.tasks.map(t => (<tr key={t.id}>
-            <td>{t.title}</td>
-          </tr>))}
+          {this.state.tasks.map(t => (<Task key={t.id} data={t}/>))}
         </tbody>
         </table>
         <p>{this.state.tasks.length} tasks</p>
       </div>
     )
+  }
+}
+
+function classNameHelper (obj) {
+  return Object.getOwnPropertyNames(obj).filter(k => obj[k]).join(' ');
+}
+
+class Task extends React.Component {
+  isDone () {
+    return this.props.data.progress === 1;
+  }
+
+  styles () {
+    var ret = {};
+    if (this.isDone()) {
+      ret.textDecoration = 'line-through';
+    }
+    return ret;
+  }
+
+  render () {
+    var data = this.props.data;
+    return (<tr style={this.styles()}>
+      <td>{data.title}</td>
+      <td>{data.prio}</td>
+      <td>{data.urg}</td>
+    </tr>);
   }
 }
